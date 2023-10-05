d1dd31a08e820aa5b29c42bf7006b6ca6032f321
calamitymandy <amandined.plateforme@gmail.com>
2023-10-05 21:54:15 +0200

    fusion with menu

--- artim-app/src/pages/index.js
+++ artim-app/src/pages/index.js
@@ -1,11 +1,9 @@
 import { PostsList } from './PostsList';
 
-
 export default function Home() {
 
   return (
     <main>
-      <Link href="/creationpost">CREATE POST</Link>
       <PostsList />
     </main>
   );
