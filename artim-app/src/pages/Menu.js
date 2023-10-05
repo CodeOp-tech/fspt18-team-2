421e25f9217b84fdef97c606aed916f77bd86c43
arema-arega <128746733+arema-arega@users.noreply.github.com>
2023-10-05 19:38:18 +0200

    Changes in Back and front including menu, libraries

---
+++ artim-app/src/pages/Menu.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { BsSearchHeart } from "react-icons/bs";
+import { TfiLayoutListPost } from "react-icons/tfi";
+import { AiFillHome } from "react-icons/ai";
+
+
+export default function Menu() {
+  return (
+    <div className='menu_grid'>
+
+      <div className='menu_item' >
+        <Link href="/">HOME</Link>
+        <AiFillHome/>
+      </div> 
+
+
+      <div className='menu_item' >
+        <Link href="/search">SEARCH</Link>
+        <BsSearchHeart/>
+      </div>   
+     
+      <div className='menu_item'>
+        <Link href="/posts">CREATE POST</Link>
+        <TfiLayoutListPost/>
+      </div>
+    </div>
+  );
+}
