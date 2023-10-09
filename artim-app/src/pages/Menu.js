88d4d8d1f062291c42672eebd70d2bb89c38ff54
Andrea Hocevar <annhocevar@gmail.com>
2023-10-09 20:54:36 +0200

    stylesForms

--- artim-app/src/pages/Menu.js
+++ artim-app/src/pages/Menu.js
@@ -2,51 +2,68 @@ import Link from "next/link";
 import { BsSearchHeart } from "react-icons/bs";
 import { TfiLayoutListPost } from "react-icons/tfi";
 import { AiFillHome } from "react-icons/ai";
-import { BiSolidUserCircle, BiSolidUserX, BiSolidUserCheck } from "react-icons/bi";
+import {
+  BiSolidUserCircle,
+  BiSolidUserX,
+  BiSolidUserCheck,
+} from "react-icons/bi";
 import { useAuth } from "../components/AuthContext";
 
 export default function Menu() {
   const { isLogged } = useAuth();
 
   return (
-    <header className="p-4 shadow-md">
-      <div className="flex gap-6 justify-end">
+    <header className="bg-white p-4 shadow-md">
+      <div className="container mx-auto flex gap-6 justify-end">
         {isLogged ? (
           <>
-            <Link href="/login" className="flex items-center gap-2">
+            <Link
+              href="/login"
+              className="flex items-center gap-2 text-pink-700"
+            >
               <BiSolidUserCheck />
-              <span>LOGOUT</span>
+              <span className="font-bold">Log Out</span>
             </Link>
-            
-            <Link className="flex items-center gap-2" href="/posts">
-          <TfiLayoutListPost />
-          <span>CREATE POST</span>
-        </Link>
+
+            <Link
+              href="/posts"
+              className="flex items-center gap-2 text-neutral-700"
+            >
+              <TfiLayoutListPost />
+              <span>Create</span>
+            </Link>
           </>
         ) : (
           <>
-            <Link href="/login" className="flex items-center gap-2">
+            <Link
+              href="/login"
+              className="flex items-center gap-2 text-teal-400"
+            >
               <BiSolidUserX />
-              <span>LOGIN</span>
+              <span>Log In</span>
             </Link>
-            <Link href="/registration" className="flex items-center gap-2">
+            <Link
+              href="/registration"
+              className="flex items-center gap-2 text-amber-400"
+            >
               <BiSolidUserCircle />
-              <span>REGISTER</span>
+              <span>Register</span>
             </Link>
           </>
         )}
 
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2 text-neutral-700">
           <AiFillHome />
-          <span>HOME</span>
+          <span>Home</span>
         </Link>
 
-        <Link href="/search" className="flex items-center gap-2">
+        <Link
+          href="/search"
+          className="flex items-center gap-2 text-neutral-700"
+        >
           <BsSearchHeart />
-          <span>SEARCH</span>
+          <span>Explore</span>
         </Link>
-
-        
       </div>
     </header>
   );
